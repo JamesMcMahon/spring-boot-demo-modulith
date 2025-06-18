ba8041b7f92786d6d7c4fe8a1a5d77fc0150b850
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-06-18 10:47:08 -0500

    Consistent casing in SQL

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE "books"
 (
-    id    BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    isbn  VARCHAR(20)  NOT NULL UNIQUE,
-    title VARCHAR(255) NOT NULL,
+    id     BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    isbn   VARCHAR(20)  NOT NULL UNIQUE,
+    title  VARCHAR(255) NOT NULL,
     author VARCHAR(255) NOT NULL
 );
 
-create table "copies"
+CREATE TABLE "copies"
 (
-    id       bigint generated always as identity primary key,
-    isbn     varchar(20)  not null,
-    location varchar(255) not null,
-    constraint fk_copies_books foreign key (isbn) references "books" (isbn)
+    id       BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    isbn     VARCHAR(20)  NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    CONSTRAINT fk_copies_books FOREIGN KEY (isbn) REFERENCES "books" (isbn)
 );
