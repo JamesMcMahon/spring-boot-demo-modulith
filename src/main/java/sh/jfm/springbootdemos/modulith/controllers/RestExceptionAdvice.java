7022fed1c3f97fd892068556c634d6eac1bc15ce
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-06-25 11:11:05 -0500

    Use enum for HTTP status

--- src/main/java/sh/jfm/springbootdemos/modulith/controllers/RestExceptionAdvice.java
+++ src/main/java/sh/jfm/springbootdemos/modulith/controllers/RestExceptionAdvice.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import sh.jfm.springbootdemos.modulith.services.*;
 
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
 /// Centralizes translation of domain exceptions to HTTP status codes,
 /// avoiding scattered try/catch in controllers.
 @ControllerAdvice
@@ -12,26 +15,26 @@ public class RestExceptionAdvice {
 
     @ExceptionHandler(BookAlreadyExistsException.class)
     ResponseEntity<Void> handleBookExists() {
-        return ResponseEntity.status(409).build();
+        return ResponseEntity.status(CONFLICT).build();
     }
 
     @ExceptionHandler(BookNotFoundException.class)
     ResponseEntity<Void> handleBookMissing() {
-        return ResponseEntity.status(404).build();
+        return ResponseEntity.status(NOT_FOUND).build();
     }
 
     @ExceptionHandler(CopyNotFoundException.class)
     ResponseEntity<Void> handleCopyMissing() {
-        return ResponseEntity.status(404).build();
+        return ResponseEntity.status(NOT_FOUND).build();
     }
 
     @ExceptionHandler(PatronNotFoundException.class)
     ResponseEntity<Void> handlePatronMissing() {
-        return ResponseEntity.status(404).build();
+        return ResponseEntity.status(NOT_FOUND).build();
     }
 
     @ExceptionHandler({NoAvailableCopiesException.class, LoanNotFoundException.class})
     ResponseEntity<Void> handleLoanConflicts() {
-        return ResponseEntity.status(409).build();
+        return ResponseEntity.status(CONFLICT).build();
     }
 }
