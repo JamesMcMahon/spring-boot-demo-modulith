6bd82293a3cfca19cf8d2fc3a779c5804e03af97
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-07-03 15:23:57 -0500

    Remove dependency on module in test

--- src/test/java/sh/jfm/springbootdemos/modulith/lending/LendingApplicationModuleTest.java
+++ src/test/java/sh/jfm/springbootdemos/modulith/lending/LendingApplicationModuleTest.java
@@ -5,34 +5,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.modulith.test.ApplicationModuleTest;
 import org.springframework.modulith.test.Scenario;
-import sh.jfm.springbootdemos.modulith.inventory.Copy;
-import sh.jfm.springbootdemos.modulith.inventory.Inventory;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import sh.jfm.springbootdemos.modulith.inventoryapi.InventoryApi;
 import sh.jfm.springbootdemos.modulith.lendingevents.ReturnCopyEvent;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.ANY;
 import static org.springframework.modulith.test.ApplicationModuleTest.BootstrapMode.ALL_DEPENDENCIES;
 
 /// Tests the interactions between the Lending module and other modules.
-@ApplicationModuleTest(mode = ALL_DEPENDENCIES, extraIncludes = "inventory")
+@ApplicationModuleTest(mode = ALL_DEPENDENCIES)
 // need to replace the database otherwise we have issues reapplying the schema
 @AutoConfigureTestDatabase(replace = ANY)
 public class LendingApplicationModuleTest {
 
     @Autowired
-    private Inventory inventory;
-    @Autowired
     private Lending lending;
+    @MockitoBean
+    private InventoryApi mockInventoryApi;
 
     /// Tests that returning a book triggers a ReturnCopyEvent.
     ///
     /// @param scenario Spring Modulith test scenario for event verification
     @Test
     void returningABookPublishesAnEvent(Scenario scenario) {
-        var loan = borrowBook(
-                lending.addPatron(new Patron("Test", "User")).id(),
-                "123"
-        );
+        when(mockInventoryApi.markNextCopyAsUnavailable("123")).thenReturn(777L);
+        var patron = lending.addPatron(new Patron("Test", "User"));
+        var loan = lending.borrow(patron.id(), "123");
+
         scenario.stimulate(() -> lending.returnBook(loan.patronId(), loan.isbn()))
                 .andWaitForEventOfType(ReturnCopyEvent.class)
                 .toArriveAndVerify(event -> assertThat(event)
@@ -40,10 +41,4 @@ public class LendingApplicationModuleTest {
                         .hasFieldOrProperty("timestamp"));
     }
 
-    @SuppressWarnings("SameParameterValue")
-    private Loan borrowBook(long patronId, String isbn) {
-        inventory.registerIsbn(isbn);
-        inventory.add(new Copy(isbn, "A-1"));
-        return lending.borrow(patronId, isbn);
-    }
 }
